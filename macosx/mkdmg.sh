ab2d0b44d14652306655d7dfddda5738d102ae03
glehmann <glehmann@362d53da-c1cb-4f1a-aa5e-931fae15fbd4>
2008-03-03 16:24:57 +0000

    associe l'icone a l'image dmg

--- macosx/mkdmg.sh
+++ macosx/mkdmg.sh
@@ -1,8 +1,14 @@
-#!/bin/sh
+#!/bin/sh -x
 
 # store the current dir. The image will be stored here.
 OUT=$PWD
 VERSION=$1
+
+if [ -z "$VERSION" ]
+then
+  echo "il faut un numéro de version"
+  exit 1
+fi
 
 # make a temp dir to create the... temp files
 mkdir tmp
@@ -12,20 +18,25 @@ pushd tmp
 svn export svn://svn.tuxfamily.org/svnroot/dvorak/svn/pilotes/trunk pilotes
 pushd pilotes/macosx
 
+rm -f "$OUT/fr-dvorak-bepo-macosx-$VERSION.dmg"
+hdiutil create "$OUT/tmp/fr-dvorak-bepo-macosx-$VERSION.dmg" -size 13m -fs HFS+ -volname "fr-dvorak-bépo ($VERSION)"
+dev_handle=`hdiutil mount "$OUT/tmp/fr-dvorak-bepo-macosx-$VERSION.dmg" | fgrep "fr-dvorak-b" | cut -f 1`
+
 # generate the alt confs
 python generate_alt.py
 
-# remove the devel files
-rm -f generate_alt.py devel.txt mkdmg.sh dead_keys.py
+# use the icon for the volume
+ditto -rsrcFork fr-dvorak-bepo.icns "/Volumes/fr-dvorak-bépo ($VERSION)/.VolumeIcon.icns"
+/Developer/Tools/SetFile -a C "/Volumes/fr-dvorak-bépo ($VERSION)"
 
 # copy tho licenses
-cp ../CC-SA-BY.txt ../GFDL.txt  .
+cp ../CC-SA-BY.txt ../GFDL.txt  "/Volumes/fr-dvorak-bépo ($VERSION)"
 
-# create the image
-hdiutil create $OUT/fr-dvorak-bepo-macosx-$VERSION.dmg \
-  -ov \
-  -srcdir . \
-  -volname "fr-dvorak-bépo ($VERSION)"
+# copy the bundle and the readmes
+cp -R fr-dvorak-bepo.bundle LISEZ_MOI.txt READ_ME.txt "/Volumes/fr-dvorak-bépo ($VERSION)"
+
+hdiutil detach $dev_handle
+hdiutil convert -imagekey zlib-level=9 -format UDZO "$OUT/tmp/fr-dvorak-bepo-macosx-$VERSION.dmg" -o "$OUT/fr-dvorak-bepo-macosx-$VERSION.dmg"
 
 popd
 popd
