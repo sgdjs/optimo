4e125f0fca728edf6ae01722a95e2abb9ce1525a
glehmann <glehmann@362d53da-c1cb-4f1a-aa5e-931fae15fbd4>
2008-03-07 23:12:47 +0000

    ajout d'un script pour generer les fichier symbols.conf de Crako

---
+++ macosx/symbolsConf.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Génération de touches mortes pour mac os 
+#
+# Copyright (C) 2008 Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+
+
+import sys, maCompose
+
+out = file("symbols.conf", "w")
+for C in sorted(maCompose.composeChars.keys()):
+  code = maCompose.composeChars[C]
+  ucode = repr(C)[2:-1]
+  if ucode.startswith(r'\u'):
+    ucode = ucode[2:].rjust(4, '0')
+    v  = (code, ucode, code, code, ucode)
+    s = "\t".join(v) + "\n"
+    out.write(s)
