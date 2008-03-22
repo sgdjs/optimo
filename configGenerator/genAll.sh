c2cbc3ee9be9e1cef433429ca6c49d258a9e3d4c
glehmann <glehmann@362d53da-c1cb-4f1a-aa5e-931fae15fbd4>
2008-03-22 15:42:45 +0000

    corrige la generation pour xorg legacy

--- configGenerator/genAll.sh
+++ configGenerator/genAll.sh
@@ -9,4 +9,4 @@ VERSION=0.6.5.1
 ./configGenerator.pl $VERSION x_compose                            > "results/layout-${VERSION}.XCompose"
 ./configGenerator.pl $VERSION win_msklc | iconv -f utf-8 -t utf-16 > "results/layout-${VERSION}.klc"
 
-sed -e '/^[^{]\+$/s:pc(pc105):pc/pc(pc105):' "results/layout-${VERSION}-user.xkb" > "results/layout-${VERSION}-user-legacy.xkb"
+perl -p -e 's#\tinclude "pc\(pc105\)"#\tinclude "pc/pc(105)"#g' "results/layout-${VERSION}-user.xkb" > "results/layout-${VERSION}-user-legacy.xkb"
