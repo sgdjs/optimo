eb02d44f865f62b55ed663b55e912a02cd394239
glehmann <glehmann@362d53da-c1cb-4f1a-aa5e-931fae15fbd4>
2008-03-26 06:53:06 +0000

    genere le .kmap dans genAll.sh

--- configGenerator/genAll.sh
+++ configGenerator/genAll.sh
@@ -12,6 +12,7 @@ VERSION=0.6.5.1
 ./map.py "results/layout-${VERSION}.xkb" "results/layout-${VERSION}.map"
 ./klavaro.py "results/layout-${VERSION}.xkb" "results/layout-${VERSION}.kbd"
 ./ktouch.py "results/layout-${VERSION}.xkb" "results/layout-${VERSION}.keyboard"
+./keymaps.py "results/layout-${VERSION}.xkb" "results/layout-${VERSION}.kmap"
 ./macosx.py "results/layout-${VERSION}.xkb" "results/layout-${VERSION}.keylayout"
 
 perl -p -e 's#\tinclude "pc\(pc105\)"#\tinclude "pc/pc(pc105)"#g' "results/layout-${VERSION}-user.xkb" > "results/layout-${VERSION}-user-legacy.xkb"
