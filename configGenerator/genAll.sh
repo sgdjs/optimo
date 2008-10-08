272b9f833420c9c86fe06d3350aa25c6a69bcff2
a2line <a2line@362d53da-c1cb-4f1a-aa5e-931fae15fbd4>
2008-10-08 06:56:18 +0000

    suppression des lignes pour pkl

--- configGenerator/genAll.sh
+++ configGenerator/genAll.sh
@@ -1,28 +1,15 @@
 #!/bin/sh
-
-#VERSION=0.1.1
 VERSION=1.0rc2
-
 ./configGenerator.pl $VERSION x_xkb_root > "results/layout-${VERSION}.xkb"
 ./configGenerator.pl $VERSION x_xkb_user > "results/layout-${VERSION}-user.xkb"
 ./configGenerator.pl $VERSION x_xmodmap  > "results/layout-${VERSION}.xmodmap"
 ./configGenerator.pl $VERSION x_compose  > "results/layout-${VERSION}.XCompose"
-
-#Fichiers pour Windows
 ./configGenerator.pl $VERSION win_msklc_azerty | iconv -f utf-8 -t utf-16le > "results/fr-dvorak-bepo-${VERSION}A-kbd.klc"
 ./configGenerator.pl $VERSION win_msklc_bepo   | iconv -f utf-8 -t utf-16le > "results/fr-dvorak-bepo-${VERSION}B-kbd.klc"
 ./configGenerator.pl $VERSION win_msklc_qwertz | iconv -f utf-8 -t utf-16le > "results/fr-dvorak-bepo-${VERSION}C-kbd.klc"
 ./configGenerator.pl $VERSION win_msklc_azerty | iconv -f utf-8 -t utf-16 > "results/fr-dvorak-bepo-${VERSION}A.klc"
 ./configGenerator.pl $VERSION win_msklc_bepo   | iconv -f utf-8 -t utf-16 > "results/fr-dvorak-bepo-${VERSION}B.klc"
 ./configGenerator.pl $VERSION win_msklc_qwertz | iconv -f utf-8 -t utf-16 > "results/fr-dvorak-bepo-${VERSION}C.klc"
-perl klc2ini.pl results/fr-dvorak-bepo-${VERSION}B.klc
-perl ini2html.pl results/layout.ini
-mv results/layout.ini results/layoutB.ini
-perl klc2ini.pl results/fr-dvorak-bepo-${VERSION}A.klc
-mv results/layout.ini results/layoutA.ini
-perl klc2ini.pl results/fr-dvorak-bepo-${VERSION}C.klc
-mv results/layout.ini results/layoutC.ini
-
 ./map.py         "results/layout-${VERSION}.xkb" "results/layout-${VERSION}.txt"
 ./svg.py         "results/layout-${VERSION}.xkb" "results/fr-dvorak-bepo-${VERSION}"
 ./klavaro.py     "results/layout-${VERSION}.xkb" "results/layout-${VERSION}.kbd"
