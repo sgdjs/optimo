c6fab0d71a9c02fff5371aa71caffc2e3ab9d740
glehmann <glehmann@362d53da-c1cb-4f1a-aa5e-931fae15fbd4>
2009-05-31 17:54:16 +0000

    suppression du BOM

--- configGenerator/configGenerator.pl
+++ configGenerator/configGenerator.pl
@@ -1,4 +1,4 @@
-﻿#!/usr/bin/env perl
+#!/usr/bin/env perl
 
 ###########################################################################
 #   Copyright (C) 2008 by Nicolas Chartier                                #
